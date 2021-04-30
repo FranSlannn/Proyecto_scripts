7ba61d88924f70acf2b66820acf8bb0ef62a1ef0
FranSlannn <slannn@hotmail.com>
2021-04-30 15:42:42 +0200

    ultimo

--- exportar.php
+++ exportar.php
@@ -1,5 +1,14 @@
 <?php 
 
+// Para la linea de comando o desde Power Shell:
+// php exportar.php -host=localhost -newsite=wordpress33 -dbname=wordpress -dbuser=root
+
+// Para el navegador:
+// http://localhost/scripts/exportar.php?-host=localhost&-newsite=wordpress33&-dbname=wordpress
+
+
+// necesario para hacer funcionar los scripts por powershell
+
     if(isset($argv)){
         foreach ($argv as $key => $arg) {
             if($key > 0){
@@ -12,40 +21,87 @@
             }
         }
     }
-    if(isset($_GET['--help'])){
-        print("Comando exportar
+
+    //rellenar con todos los campos que queramos; es la ayuda que vera el usuario
+    if(isset($_GET['-help'])){ //necesario el --help para funcionar
+        print("Comando exportar.php
         Añade los siguientes argumentos:
-            -host=host: Indica el host de la base de datos de wordpress a exportar
+            -host=localhost: Indica el host de la base de datos de wordpress a exportar
+            -dbuser=root: Indica el nombre del usuario de la base de datos
+            -pass=password Indica la clave del usuario
+            -dbname=wordpress Indica el nombre de la base de datos de wordpress
+            -newsite=wordpress33 Indica el nombre de la nueva pagina de wordpress
+            
         ");
         die;
     }
-//exportar el sql
-    $new_site = 'wordpress33';
-    $comprimido = 'wordpress_bkp.zip';
 
-    //ENTER THE RELEVANT INFO BELOW
-    if(isset($_GET['-dbuser'])){
+    // host
+    if(isset($_GET['-host'])) {  //servidor
+        $mysqlHostName = $_GET['-host']; //'localhost';
+    } else {//ejemplo de como se puede cambiar los datos en el powershell
+        print("El argumento -host es obligatorio y debe indicar el nombre del servidor. Ej
+        -host=localhost
+        ");
+        die;
+    }
+
+    //exportar el sql
+    if(isset($_GET['-newsite'])) { //nombre del nuevo sitio de wordpress
+        $newsite = $_GET['-newsite']; //'wordpress33';
+    } else {//ejemplo de como se puede cambiar los datos en el powershell
+        print("El argumento -newsite es obligatorio y debe indicar el nombre de la copia de la base de datos. Ej
+        -newsite=wordpress33
+        ");
+        die;
+    }
+
+    if(isset($_GET['-dbname'])) { //nombre de la base de datos
+        $DbName = $_GET['-dbname']; //'wordpress';
+        $comprimido = $_GET['-dbname'] . '_bkp.zip';
+    } else {//ejemplo de como se puede cambiar los datos en el powershell
+        print("El argumento -dnname es obligatorio y debe indicar el nombre de la base de datos. Ej
+        -newsite=wordpress
+        ");
+        die;
+    }
+
+    //informacion relevante
+    if(isset($_GET['-dbuser'])) {
         $mysqlUserName      = $_GET['-dbuser'];
-    }else{
-        print("El argumento -dbuser es obligatoro y debe indicar el usiao de la base de datos. Ej
+    } else {//ejemplo de como se puede cambiar los datos en el powershell
+        print("El argumento -dbuser es obligatorio y debe indicar el usuario de la base de datos. Ej
         -dbuser=root
         ");
         die;
     }
-    $mysqlPassword      = "";
-    $mysqlHostName      = "localhost";
-    $DbName             = "wordpress";
-    $backup_name        = "mybackup.sql";
-    $tables             = "Your tables";
-    // Extender tiempo para garantizar ejecución completa de compresión
+
+    if(isset($_GET['-pass'])) {
+        $mysqlPassword = $_GET['-pass'];
+    } else {
+        print("El argumento -pass es obligatorio y debe indicar la clave del usuario. Ej
+        -pass=hola
+        ");
+        die;
+    }
+	//datos importantes
+	
+		//$mysqlPassword      = "";
+		//$mysqlHostName      = "localhost";
+		//$DbName             = "wordpress";
+		//$newsite            = 'wordpress33';
+		$backup_name        = "mybackup.sql";
+		$tables             = "Your tables";
+    
+    // Extiende tiempo para garantizar ejecución completa de compresión
     ini_set('max_execution_time', '300'); //300 seconds = 5 minutes
     set_time_limit(300);
 
    //or add 5th parameter(array) of specific tables:    array("mytable1","mytable2","mytable3") for multiple tables
 
-    Export_Database($mysqlHostName,$mysqlUserName,$mysqlPassword,$DbName,  $tables=false, $backup_name, $mysqlHostName, $DbName, $new_site);
+    Export_Database($mysqlHostName,$mysqlUserName,$mysqlPassword,$DbName,  $tables=false, $backup_name, $mysqlHostName, $DbName, $newsite);
 
-    function Export_Database($host,$user,$pass,$name,  $tables=false, $backup_name=false, $mysqlHostName, $DbName, $new_site)
+    function Export_Database($host,$user,$pass,$name,  $tables=false, $backup_name=false, $mysqlHostName, $DbName, $newsite)
     {
         $mysqli = new mysqli($host,$user,$pass,$name); 
         $mysqli->select_db($name); 
@@ -111,11 +167,14 @@
             } $content .="\n\n\n";
         }
 
-        // Reemplazar en $content /localhost/wordpress por  '/localhost/' . $new_site
-        $content = str_replace($mysqlHostName . '/' . $DbName, $mysqlHostName . '/' . $new_site, $content);
-        $content.="SET FOREIGN_KEY_CHECKS=1;\n\n";
+        // Reemplazar en $content /localhost/wordpress por  '/localhost/' . $newsite
+        $content  = str_replace($mysqlHostName . '/' . $DbName, $mysqlHostName . '/' . $newsite, $content);
+        $content  = str_replace('0000-00-00 00:00:00', '', $content); //buscar y que esta vacio esto -----> NOT NULL DEFAULT '0000-00-00 00:00:00'
+        $content .= "SET FOREIGN_KEY_CHECKS=1;\n\n"; //arregla errores tablas sql
+        
+
         if(file_put_contents( $backup_name, $content)) {
-            echo "Descarga exitosa " . "<br />";
+            echo "Descarga exitosa " . "<br />";s
         } else {
             echo "Descarga de archivo fallida." . "<br />";
         }
@@ -151,7 +210,7 @@
     }
     $zip->addFile('./mybackup.sql', 'sql/mybackup.sql');
     
-    // Zip archivo sera creado despues de cerrar
+    // Archivo Zip sera creado despues de cerrar
     $resultado = $zip->close();
     if ($resultado) {
         echo  "<br />" . " Archivo zip creado con éxito";
--- importar.php
+++ importar.php
@@ -1,39 +1,110 @@
 <?php
 
-$comprimido = 'wordpress_bkp.zip';
-$destino = '../wordpress33';
-$copia = '/sql/mybackup.sql';   // debe estar dentro de $destino: $destino . $copia
 
-$db_host = 'localhost';
-$db_uname = 'root';  //usuario
-$db_password = ''; //contraseña
-$db = 'wordpress33'; //nombre base de datos 
-$file_path = $destino . $copia; //donde esta el backup del sql
+// Para la linea de comando o desde Power Shell:
+// php importar.php -host=localhost -newsite=wordpress33 -dbuser=root -pass=contraseña
 
-// Extender tiempo para garantizar ejecución completa de compresión
-ini_set('max_execution_time', '300'); //300 seconds = 5 minutes
-set_time_limit(300);
+// Para el navegador:
+// http://localhost/scripts/importar.php?-host=localhost&-newsite=wordpress33&-dbname=wordpress
 
-//descomprimirmos el zip
-$zip = new ZipArchive;
-if ($zip->open($comprimido) === TRUE) {
-    $zip->extractTo($destino);
-    $zip->close();
-    echo 'ok';
-} else {
-    echo 'fallo';
+
+
+// necesario para hacer funcionar los scripts por powershell
+
+if(isset($argv)){
+    foreach ($argv as $key => $arg) {
+        if($key > 0){
+            $e=explode("=",$arg);
+            if(count($e)==2){
+                $_GET[$e[0]]=$e[1];
+            }else {
+                $_GET[$e[0]]=0;
+            }
+        }
+    }
 }
 
-/*$options = array(
-    'db_uname' => 'root',  //usuario
-    'db_password' => '', //contraseña
-    'db' => 'wordpress33', //nombre base de datos 
-    'file_path' => $destino . $copia //donde esta el backup del sql
-);*/
+    //rellenar con todos los campos que queramos
+    if(isset($_GET['-help'])){ //necesario el --help para funcionar
+        print("Comando importar.php
+        Añade los siguientes argumentos:
+            -host=localhost: Indica el host de la base de datos de wordpress a exportar
+            -dbuser=root: Indica el nombre del usuario de la base de datos
+            -pass=password Indica la clave del usuario            
+            -newsite=wordpress33 Indica el nombre de la nueva pagina de wordpress
+        ");
+        die;
+    }
 
-$conn2 = mysqli_connect($db_host, $db_uname, $db_password);
-mysqli_query($conn2, "create database " . $db.";");
-$conn = mysqli_connect($db_host, $db_uname, $db_password, $db);
+    // host
+    if(isset($_GET['-host'])) { //servidor
+        $mysqlHostName = $_GET['-host']; //'localhost';
+    } else {//ejemplo de como se puede cambiar los datos en el powershell
+        print("El argumento -host es obligatorio y debe indicar el nombre del servidor. Ej
+        -host=localhost
+        ");
+        die;
+    }
+
+    //importar el sql
+    if(isset($_GET['-newsite'])) {  //nombre del nuevo sitio de wordpress
+        $newsite = $_GET['-newsite']; //'wordpress33';
+    } else {//ejemplo de como se puede cambiar los datos en el powershell
+        print("El argumento -newsite es obligatorio y debe indicar el nombre de la nueva base de datos. Ej
+        -newsite=wordpress33
+        ");
+        die;
+    }
+
+   
+    //informacion relevante
+    if(isset($_GET['-dbuser'])) {
+        $mysqlUserName      = $_GET['-dbuser'];
+    } else {//ejemplo de como se puede cambiar los datos en el powershell
+        print("El argumento -dbuser es obligatorio y debe indicar el usuario de la base de datos. Ej
+        -dbuser=root
+        ");
+        die;
+    }
+
+    if(isset($_GET['-pass'])) {
+        $mysqlPassword = $_GET['-pass'];
+    } else {
+        print("El argumento -pass es obligatorio y debe indicar la clave del usuario. Ej
+        -pass=hola
+        ");
+        die;
+    }
+
+
+$comprimido = 'wordpress_bkp.zip';
+$destino = '../wordpress33';
+$copia = '/sql/mybackup.sql';   // debe estar dentro de $destino: $destino . $copiaç
+
+//datos importantes
+	//$host = 'localhost';
+	//$dbuser = 'root';  //usuario
+	//$pass = ''; //contraseña
+	//$newsite = 'wordpress33'; //nombre base de datos 
+	$file_path = $destino . $copia; //donde esta el backup del sql
+
+//Extender tiempo para garantizar ejecución completa de compresión
+	ini_set('max_execution_time', '300'); //300 seconds = 5 minutes
+	set_time_limit(300);
+
+//Descomprimirmos el zip
+	$zip = new ZipArchive;
+	if ($zip->open($comprimido) === TRUE) {
+		$zip->extractTo($destino);
+		$zip->close();
+		echo 'ok';
+	} else {
+		echo 'fallo';
+	}
+
+$conn2 = mysqli_connect($host, $dbuser, $pass);
+mysqli_query($conn2, "create database " . $newsite.";");
+$conn = mysqli_connect($host, $dbuser, $pass, $newsite);
 
 function restoreMysqlDB($filePath, $conn){
     $sql = '';
